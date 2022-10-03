49a75e645ac8119a6c4103399e5805c7a46f2887
jnissenbaum18 <jnissenbaum@verizon.net>
2022-10-03 10:38:20 -0400

    updates to master

--- routes/blogs.js
+++ routes/blogs.js
@@ -4,12 +4,15 @@ var router = express.Router();
 
 const {db} = require("../mongo")
 
-router.get('/get-single', async  function(req, res, next) {
+router.get('/get-one-example', async function(req, res, next) {
 	const blogPosts = await db().collection("posts").findOne({
 		id: {
 			$exists: true
 		}
 	})
+
+	console.log(blogPosts)
+
   res.json({
 		success: true,
 		posts: blogPosts
